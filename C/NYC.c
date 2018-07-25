ca5f04e0bccd1628951219d660101f262338951c
SubhajitCode <41638148+SubhajitCode@users.noreply.github.com>
2018-07-25 16:07:07 +0530

    Update NYC.c

--- C/NYC.c
+++ C/NYC.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<string.h>
-void main()
+long int findbribe(long int q[],long int qcount);
+
+int main()
 {   int t;
     scanf("%d",&t);
     int i;
@@ -16,43 +18,51 @@ void main()
         {     
             scanf("%ld",&q[j]);
         }
-	long int pass = n[i];
+    long int pass = n[i];
 
-	long int result ;
-	result = findbribe(q,pass);
-	printf("%ld",result);
+    long int result ;
+    result = findbribe(q,pass);
+        if(result==-1)
+        {
+            printf("Too chaotic\n");
+        }
+        else
+        {
+            printf("%ld\n",result);
+        }
     }
+ return 0;
 
 }
 long int findbribe(long int q[],long int qcount)
 {
-	long int i=0,r=0;
-	long int q2[qcount];
-	long int qr[qcount];
-	for(i=0;i<qcount;i++)
-	{
-		q2[i]=(i+1);
-	}
-	for(i=0;i<qcount;i++)
+    long int i=0,r=0;
+    long int q2[qcount];
+    long int qr[qcount];
+    for(i=0;i<qcount;i++)
+    {
+        q2[i]=(i+1);
+    }
+    for(i=0;i<qcount;i++)
         {
                 qr[i]=(q2[i]-*(q+i));
         }
-	for(i=0;i<qcount;i++)
+    for(i=0;i<qcount;i++)
         {
-		
+        
                 if(qr[i]<0)
-		{
-			if(qr[i]>=-2)
-			{
-				r=r+qr[i];
-			}
-			else
-			{
-				return -1;
-			}
-		}
+        {
+            if(qr[i]>=-2)
+            {
+                r=r+qr[i];
+            }
+            else
+            {
+                return -1;
+            }
         }
-	return r;
+        }
+    return (-r);
 
 
 }
